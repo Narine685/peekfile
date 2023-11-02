0c392ad6184802cf7586ca966772f91d6e9efb00
Narine685 <narine.fischer@autonoma.cat>
2023-11-02 02:15:37 +0100

    peek.sh

--- peek.sh
+++ peek.sh
@@ -1,3 +1,16 @@
-head -n $2 $1 
-echo ...
-tail -n $2 $1 
+if [[ -n $2 ]];
+then
+  lines=$2
+else
+  lines=3
+fi
+
+if [[ $(cat $1 | wc -l) -le $(( $lines * 2 )) ]]
+then
+  cat $1
+else
+  echo "Warning: the file passed has more lines than two times the lines specified or the default lines (3), not all lines will be printed"
+  head -n $lines $1
+  echo ...
+  tail -n $lines $1
+fi
