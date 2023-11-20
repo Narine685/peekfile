cb53694ab1ffd6ce52a35eef1c00c63a6ac52b4e
Narine685 <narine.fischer@autonoma.cat>
2023-11-20 18:44:40 +0100

    fastascan.sh

--- fastascan.sh
+++ fastascan.sh
@@ -20,12 +20,31 @@ echo -e "Calculating the number of files obtained:\nThere are $(echo "$paths" | 
 echo -e "Calculating the number of unique IDs in the totality of fasta and fa files found:" # echo to indicate the step in which the script is
 for path in $paths # this loop will be used to save ALL the IDs (first words of the entries heads) in a file 
     do if [[ $(grep ">" $path|wc -l) -gt 0 ]] #this step is used to make sure that the files used are text, not binary
-        then grep ">" $path | awk '{print $1}' >> IDs
+        then 
+            grep ">" $path | awk '{print $1}' >> IDs
     fi
 done
-echo -e "There are $(sort IDs |uniq -c | wc -l) unique IDs in the totality of fasta and fa files in $1\n" #we print the number of unique IDs in the file with ALL IDs
+echo -e "There are $(sort IDs |uniq -c | wc -l) unique IDs in the totality of fasta and fa files in $1\n" # print the number of unique IDs in the file with ALL IDs
 
 
+#obtaining a summary of each file
+for path in $path
+    do if [[ $(grep ">" $path|wc -l) -gt 0 ]]
+    then
+        echo "=== $path ===" # print which file we are giving information about
+        if [[ -h $path ]] # with this condition, the user will get the information whether the file is a symbolic link or not 
+        then 
+            echo "This file is a symlink."
+        else
+            echo "This file is not a symlink."
+        fi
+        echo "There are $(grep -c ">" $path) sequences inside." # obtaining the number of sequences inside the file
+        total_residus=$(awk '!/>/ && length($0)> 0{gsub(/[-"\n"" "]/, "", $0)} !/>/ && length($0)> 0{len+=length($0)}END{print len}' $path) # here we modify the lines that don't start with > (sequence lines) so that the lines don't have gaps, spaces or new line characters, then, with the modified lines, a variable called len will store the added value of each sequence line, obtaining this way the total number of nucleotide or aminoacid residus of teh file, and this number will be stores in the variable total_residus.
+        echo "In this file, there are $total_residus nucleotides or aminoacids"
+    fi
+done 
+
+            
 
 
 
